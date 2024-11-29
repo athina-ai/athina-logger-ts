6865e3f45e722402ce068314936e3c7d5ed4a046
Cody Christ <cchrist1210@gmail.com>
2024-11-29 14:07:20 -0800

    Fix ESLint issues

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tseslintParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  eslint.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tseslintParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        console: 'readonly',
+        // Add other Node.js globals if needed
+        process: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'prettier': prettierPlugin,
+    },
+    rules: {
+      ...tseslint.configs['recommended'].rules,
+      'prettier/prettier': 'error',
+      // Add any custom rules here
+    },
+  },
+];
