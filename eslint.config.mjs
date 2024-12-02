95af412eb5c8498434086f1e84ef01e9a5d8b2cf
Cody Christ <cchrist1210@gmail.com>
2024-12-02 12:47:04 -0800

    Update ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,7 +28,7 @@ export default [
     rules: {
       ...tseslint.configs['recommended'].rules,
       'prettier/prettier': 'error',
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
       // Add any custom rules here
     },
   },
